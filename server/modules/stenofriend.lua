db7e213eee26c4d6330e195c50e0ab2a6e599d41
Jael Clark <jaelsonicboom@gmail.com>
2018-12-13 16:47:35 -0600

    Merged autocolor into stenofriend.

--- server/modules/stenofriend.lua
+++ server/modules/stenofriend.lua
@@ -1,3 +1,4 @@
+--Stenofriend. All about making messages look nice.
 local process = ...
 
 local stenofriend = {}
@@ -5,6 +6,7 @@ local stenofriend = {}
 function stenofriend:init()
 	process:registerCallback(self,"emote_received",1,self.handle)
 	process:registerCallback(self,"music_received",1,self.music)
+	process:registerCallback(self,"emote",3,self.autocolor)
 end
 
 function stenofriend:handle(sender, receiver, emote)
@@ -49,6 +51,25 @@ function stenofriend:fix(text) --Taken from my custom client. Fixes all common e
 	if t:find("^%s*$") then t = " " end
 
 	return t
+end
+
+function stenofriend:autocolor(client, emote)
+	local msg = emote.dialogue
+
+	--Color message
+	local tc = emote.text_color
+	if tc == COLOR_WHITE then --Change only if player hasn't.
+		if msg:find("^%([^%(].-[^%)]%)$") then  --(Thinking brackets = blue)!
+			emote.text_color = COLOR_BLUE
+		end
+		if msg:find("^%*.-%*$") then --*Is Orange*
+			emote.text_color = COLOR_ORANGE
+		end 
+		if msg:find("^%s-[%=%-%~][%=%-%~]+.-[%=%-%~][%=%-%~]+%s-$") then --Start and end cards--
+			emote.text_color = COLOR_ORANGE
+			--emote.text_centered = true
+		end
+	end
 end
 
 return stenofriend
