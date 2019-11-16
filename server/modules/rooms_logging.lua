fe4c7a2fb78d8040190297147c9bc3a159ead12c
Jael Clark <jaelsonicboom@gmail.com>
2019-11-15 21:32:15 -0600

    /roominfo command added to rooms_logging

--- server/modules/rooms_logging.lua
+++ server/modules/rooms_logging.lua
@@ -1,7 +1,11 @@
 local process = ...
 
 local rooms = {
-	name = "Rooms"
+	name = "Rooms",
+
+	help = {
+		{"areainfo","[id]","Shows info about your current room.","Aliases: '/roominfo', '/ai', '/ri'"},
+	}
 }
 
 function rooms:init()
@@ -22,6 +26,7 @@ function rooms:init()
 	process:registerCallback(self,"player_done", 3,self.connected)
 	process:registerCallback(self,"player_move", 3,self.welcometoroom)
 	process:registerCallback(self,"player_leave", 3,self.disconnected)
+	process:registerCallback(self,"command", 3,self.command)
 end
 
 function rooms:welcometoroom(client,room)
@@ -30,6 +35,49 @@ function rooms:welcometoroom(client,room)
 
 	if client.room then
 		process:sendMessage(client.room,client:getIdent().." left to "..tostring(room.name)..".")
+	end
+end
+
+function rooms:command(client, cmd,str,args)
+	if not self.parent then return end
+	if cmd == "areainfo" or cmd == "roominfo" or cmd == "ai" or cmd == "ri" then
+
+		local id = args[1]
+		if not id then
+			for k,v in pairs(self.parent.rooms) do
+				if k == id or k == tonumber(id) then
+					id = k
+					break
+				end
+			end
+		end
+		local room = self.parent.rooms[id]
+		if not room then
+			process:sendMessage(client,"Couldn't find area with that ID!")
+		end
+		local msg = "~~Area Info~~"
+		msg=msg.."\nName: '"..tostring(room.name).."'"
+		if room.basename and room.basename ~= room.name then
+			msg=msg.." (Renamed)"
+			msg=msg.."\nOriginal Name: '"..room.basename.."'"
+		end
+		msg=msg.."\nDescription: '"..tostring(room.desc).."'"
+		msg=msg.."\nPlayers: "..tostring(room.count)
+		msg=msg.."\nStatus: "..tostring(room.status)
+		msg=msg.."\nBackground: '"..tostring(room.bg).."'"
+		msg=msg.."\nMusic: '"..tostring(room.music).."'"
+		if client.room == room and room.lock then
+			msg=msg.."\nPasscode: '"..tostring(room.lock).."'"
+		end
+		if room.iniswap then
+			msg=msg.."\nIniswap: Allowed"
+		end
+		if room.modlock then
+			msg=msg.."\nLocked for Moderators."
+		end
+
+		process:sendMessage(client,msg)
+		return true
 	end
 end
 
