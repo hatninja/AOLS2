3b42c132a1b36f432d06d6bcaeae2b958ba9ab28
Jael Clark <jaelsonicboom@gmail.com>
2020-11-02 00:15:52 -0600

    Musichelper added. Looping always on.

--- server/modules/musichelper.lua
+++ server/modules/musichelper.lua
@@ -1,23 +1,28 @@
 local process = ...
 
-local rooms = {
+local music = {
 	name = "Music",
 }
 
-function rooms:init()
-	self.parent = process.modules["rooms"]
-
+--TODO: Move process looping mechanism here.
+function music:init()
 	process:registerCallback(self,"command", 3,self.command)
 	process:registerCallback(self,"music_play", 5,self.musicbutton)
+	process:registerCallback(self,"music_received", 5,self.musicreceived)
 end
 
-function rooms:musicbutton(client, music)
+function music:musicbutton(client, music)
 	music.looping = true
 end
 
-function rooms:command(client, cmd,str,args)
-	if cmd == "play" then
-	end
+function music:command(client, cmd,str,args)
+	--if cmd == "play" then
+	--end
 end
 
-return rooms
+function music:musicreceived(client, music)
+	music.looping = true
+end
+
+
+return music
